2d8e9b3907761e7cc0f0388a340cb179c87cc987
Jayesh Bhatia <jayesh.bhatia@zeuslearning.com>
2023-11-10 12:41:34 +0530

    added footer section

--- App.jsx
+++ App.jsx
@@ -1,12 +1,14 @@
 import React from "react"
 import Navbar from "./components/Navbar/Navbar"
 import Login from "./components/Login/Login"
+import Footer from "./components/Footer/Footer"
 
 export default function App() {
     return (
         <div>
             <Navbar />
             <Login />
+            <Footer />
         </div>
     )
 }
---
+++ components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css"
+
+export default function Footer() {
+    return (
+        <footer>
+            <h4 className="footer--text">About</h4>
+            <h5 className="footer-separator"> | </h5>
+            <h4 className="footer--text">Contact Us</h4>
+        </footer>
+    );
+}
